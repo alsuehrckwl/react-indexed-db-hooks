7923e4f57977ea71f70f70a6775af1de64d0b5e6
jwkim <jwkim@rodaloft.com>
2020-04-02 22:15:26 +0900

    observeTransaction add

---
+++ src/ObserveTransaction.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {
+  ObserveConsumer,
+  IndexedDBStateValue,
+  IndexedDBContext,
+} from './indexedDBProvider';
+import {useIndexedDB} from './useIndexedDB';
+import {useState} from 'react';
+import {useEffect} from 'react';
+
+function ObserveTransaction({
+  children,
+  schema,
+  index,
+  value,
+  loading = false,
+  loadingComponent = null,
+}) {
+  const [result, setResult] = useState(null);
+  const {findAll} = useIndexedDB();
+  const [state, dispatch] = IndexedDBStateValue(IndexedDBContext);
+
+  useEffect(() => {
+    findAll(schema).then(success => {
+      setResult(success.filter(item => item[index] === value));
+    });
+  }, []);
+
+  return (
+    <ObserveConsumer>
+      {({success, error, isTransactionCall}) => {
+        if (isTransactionCall && success) {
+          findAll(schema).then(success => {
+            const data = success.filter(item => item[index] === value);
+            console.log(data);
+            setResult(data);
+
+            dispatch({
+              type: 'finishTransaction',
+            });
+          });
+        }
+
+        if (isTransactionCall && loading) {
+          return loadingComponent;
+        } else {
+          return children({result});
+        }
+      }}
+    </ObserveConsumer>
+  );
+}
+
+export default ObserveTransaction;
